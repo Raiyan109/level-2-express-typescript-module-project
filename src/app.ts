c37df8ccf99af22eab7b23634c0b59e611d91ef4
Raiyan109 <kraiyan109@gmail.com>
2024-06-03 20:29:25 +0600

    Building...1 done

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cors());
 // application routes
 // app.use('/api/v1/students', StudentRoutes);
 // app.use('/api/v1/users', UserRoutes);
-// application routes
+
 app.use('/api/v1', router);
 
 const getAController = (req: Request, res: Response) => {
